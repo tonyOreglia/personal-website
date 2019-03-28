627778a45f0e6b5563fbbe61a01b41e2709791fb
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-03-28 20:39:28 +1100

    build pipeline test three

--- src/index.js
+++ src/index.js
@@ -378,7 +378,7 @@ class Game extends React.Component {
           break
         }
         this.setState({
-          engineName: `Running Engine - ${msgTokens.slice(1).join(" ")} (thalita is super beautiful)`
+          engineName: `Running Engine - ${msgTokens.slice(1).join(" ")} (thalita is really super beautiful)`
         });
         break;
       case "author":
