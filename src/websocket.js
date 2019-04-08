3dc4f9e923ec701e2b11ad71f452923c904a3be3
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-04-08 21:09:56 +1000

    update to websocket library

--- src/websocket.js
+++ src/websocket.js
@@ -1,5 +1,11 @@
+const WebSocket = require('ws');
+
 export function websocketConnect(url) {
-  const ws = new WebSocket(url);
+  const ws = new WebSocket(url, {
+    origin: "glee-git-master.tony-oreglia.now.sh",
+    protocolVersion: 8,
+    rejectUnauthorized: false
+  });
   ws.addEventListener('open', (event) => {
     ws.send('uci');
   });
