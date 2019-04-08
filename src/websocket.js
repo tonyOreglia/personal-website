a50445fa7d6842399b86b4352cdcf28a51f9a5f3
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-04-08 21:11:33 +1000

    revert

--- src/websocket.js
+++ src/websocket.js
@@ -1,11 +1,5 @@
-const WebSocket = require('ws');
-
 export function websocketConnect(url) {
-  const ws = new WebSocket(url, {
-    origin: "glee-git-master.tony-oreglia.now.sh",
-    protocolVersion: 8,
-    rejectUnauthorized: false
-  });
+  const ws = new WebSocket(url);
   ws.addEventListener('open', (event) => {
     ws.send('uci');
   });
