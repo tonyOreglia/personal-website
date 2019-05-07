7cc4f9e0a9dee28f5ac865379caea6da11b760c7
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-05-07 21:02:10 +1000

    added missing files

---
+++ src/board.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Square from "./square"
+import Chess from 'chess.js';
+import { indexToAlgebraic } from './chess/chess'
+
+export default class Board extends React.Component {
+  renderSquare(i, value) {
+    let selected = i === this.props.selected;
+    return (
+      <Square
+        key={i}
+        value={value}
+        onClick={() => this.props.onClick(i)}
+        index={i}
+        selected={selected}
+      />
+    );
+  }
+
+  render() {
+    const position = Chess();
+    position.load(this.props.position);
+    let sqs;
+    let rows = [];
+    let index;
+    for (let i = 0; i < 8; i++) {
+      sqs = [];
+      for (let j = 0; j < 8; j++) {
+        index = j + i*8;
+        sqs.push(this.renderSquare(index, position.get(indexToAlgebraic(index))));
+      }
+      rows.push(
+      <div key={i} className="board-row">
+        {sqs}
+      </div>
+      )
+    }
+    return (
+      <div>
+        {rows}
+      </div>
+    );
+  }
+}
