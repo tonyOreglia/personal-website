e68078b6f98cc7d1d6c88a2eaaf01e78dd40f434
Tony Oreglia <tony.oreglia@gmail.com>
2019-12-25 21:05:39 +0000

    update about me section

--- src/data/about.js
+++ src/data/about.js
@@ -1,7 +1,7 @@
 export const markdown = `
 #### Intro
 Hello :) I'm from San Jose, California. 
-
+<br><br>
 #### I like
 - Programming/tech 
 - Rugby
@@ -10,19 +10,18 @@ Hello :) I'm from San Jose, California.
 - Surfing
 - Chess
 - Wrestling
-
+<br><br>
 I do not often balance my interests very well. 
 Sometimes I only watch youtube videos of Slavoj Žižek and then regret doing nothing productive. But that guys is really fun to watch.
-
+<br><br>
 In 2015 I moved to Sydney, Australia where I lived until moving to Lisbon, Portugal in 2019. 
-
 <br><br>
 I have two older sisters that are pretty cool and a younger brother that is an absolute legend.
 <br><br>
 My parents drink too much wine now that we are moved out and really love their three grandkids.
 <br><br>
 My girlfriend is the light of my life. I have never even for a moment been angry or annoyed at her. 
-She is in marketing, you can contact here from her website [HERE](https://www.thalitamilan.com/). 
+She freelances in content marketing, you can reach out from her website [HERE](https://www.thalitamilan.com/).
 <br><br>
 #### Currently
 Living in Lisbon, Portugal working for a great company called Dashlane. Sign up and protect yourself from hackers, Facebook, Trump and other evil entities described on the news.
