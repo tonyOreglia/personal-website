857b8eb3a6b4d1512085c207a1bab27ccd15a77e
Tony Oreglia <tony.oreglia@gmail.com>
2020-01-07 21:26:31 +0000

    update about.js

--- src/data/about.js
+++ src/data/about.js
@@ -2,28 +2,25 @@ export const markdown = `
 #### Intro
 Hello :) I'm from San Jose, California. 
 <br><br>
-#### I like
-- Programming/tech 
-- Rugby
-- Reading/learning
-- Beer/being with friends
-- Surfing
-- Chess
-- Wrestling
-<br><br>
-I do not often balance my interests very well. 
-Sometimes I only watch youtube videos of Slavoj Žižek and then regret doing nothing productive. But that guys is really fun to watch.
-<br><br>
-In 2015 I moved to Sydney, Australia where I lived until moving to Lisbon, Portugal in 2019. 
+In 2015 I moved to Australia where I lived until moving to Portugal in 2019. 
 <br><br>
 I have two older sisters that are pretty cool and a younger brother that is an absolute legend.
 <br><br>
-My parents drink too much wine now that we are moved out and really love their three grandkids.
+My girlfriend freelances in content marketing, you can reach out [HERE](https://www.thalitamilan.com/).
 <br><br>
-My girlfriend is the light of my life. I have never even for a moment been angry or annoyed at her. 
-She freelances in content marketing, you can reach out from her website [HERE](https://www.thalitamilan.com/).
-<br><br>
+My Mom has an awesome blog, it is largely unfiltered: [Living in the Gap](https://cheryloreglia.blog/)
+My Dad played football for the [Santa Clara Broncos](https://www.youtube.com/watch?v=aWlCcuQk6y8&t=10012s) back when men didn't wear Uggs. 
+He now wears Uggs. 
 #### Currently
-Living in Lisbon, Portugal working for a great company called Dashlane. Sign up and protect yourself from hackers, Facebook, Trump and other evil entities described on the news.
+Living in Lisbon, Portugal working for a great company called [Dashlane](https://www.dashlane.com/).
+Sign up and start Dashing to protect yourself from hackers, Facebook, Trump and other evil entities described on the news.
 <br><br>
+#### I like
+- Technology
+- Rugby
+- Reading and discussing History, Philosophy, Politics, Economics
+- Beer
+- Surfing
+- Chess
+- Wrestling
 `
