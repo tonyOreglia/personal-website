50c95b579b56f8e3183e47ea3f8acdf19ebecc11
Tony Oreglia <tony.oreglia@dashlane.com>
2021-01-18 22:00:34 +0000

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'getting latest...'
+git pull
+echo 'installing libs...'
+npm i
+echo 'building...'
+npm run build
+echo 'moving artifacts to hosting folder...'
+rm -rf /root/tonycodes.com/*
+mv build/* /root/tonycodes.com/
